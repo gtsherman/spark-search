4a57bce6262598330725e9f6fdf1197629b6bc12
Garrick Sherman <gsherma2@illinois.edu>
2018-08-13 16:58:23 -0700

    fixed TrecWebParser
    The TrecWebParser was falling down due to malformed document contents
    (for example, the final doc in WTX001/B01 ends in the middle of an open
    anchor tag). While the lxml parser was able to handle the malformed html
    better than the default html.parser, it was somehow missing several docs
    while parsing the full trecweb formatted file. This new approach uses
    simple string manipulations to split apart the individual <DOC>
    elements, then stores only the contents of the files, without the
    <DOCHDR> or any other elements.

--- preprocess.py
+++ preprocess.py
@@ -85,6 +85,7 @@ class TrecTextParser(Parser):
         """
         :param all_documents: The (file_name, file_contents) RDD returned by wholeTextFiles
         :return: A (docno, doctext) RDD
+
         """
         return all_documents \
             .flatMap(lambda whole_doc: BeautifulSoup(whole_doc[1], 'html.parser').find_all('doc')) \
@@ -111,9 +112,38 @@ class TrecTextParser(Parser):
 
 
 class TrecWebParser(TrecTextParser):
+    def _docno(self, doc):
+        docno_start_tag = '<DOCNO>'
+        docno_end_tag = '</DOCNO>'
+
+        docno_start = doc.index(docno_start_tag) + len(docno_start_tag)
+        docno_end = doc.index(docno_end_tag)
+
+        return doc[docno_start:docno_end].strip().upper()
+
+    def _contents(self, doc):
+        dochdr_end_tag = '</DOCHDR>'
+
+        contents_start = doc.index(dochdr_end_tag) + len(dochdr_end_tag)
+
+        return doc[contents_start:]
+
+    def _flattened(self, doc):
+        return self._docno(doc), self._contents(doc)
+
+    def split(self, all_documents):
+        """
+        :param all_documents: The (file_name, file_contents) RDD returned by wholeTextFiles
+        :return: A (docno, doctext) RDD
+
+        """
+        return all_documents \
+            .flatMap(lambda whole_doc: whole_doc[1].replace('</DOC>', '').split('<DOC>')[1:]) \
+            .map(self._flattened)
+
     @staticmethod
     def extract(document):
-        return BeautifulSoup(document, 'html.parser').dochdr.next_siblings
+        return BeautifulSoup(document, 'lxml')
 
 
 parsers = {
